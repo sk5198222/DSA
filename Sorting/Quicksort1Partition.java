a63e39febfa4a52985dbbdeab6476850d4566976
Satyam <sksk5198222@gmail.com>
2025-08-18 22:42:39 +0530

    fixed imports of Patterns

--- Sorting/Quicksort1Partition.java
+++ Sorting/Quicksort1Partition.java
@@ -48,8 +48,6 @@ public class Quicksort1Partition {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
         List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
             .collect(toList());
